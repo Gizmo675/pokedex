6bcefb49cd514b56bfafbc910d254ddea179b119
Romain <septier.romain@gmail.com>
2020-09-18 15:08:13 +0200

    fetching all pokemon details on home page

--- src/App.js
+++ src/App.js
@@ -14,13 +14,50 @@ function App() {
 
   const [pokemonData, SetPokemonData] = useState()
   const [pokemon, SetPokemon] = useState([])
+  const [allPokemon, SetAllPokemon] = useState([])
 
-  async function pokemonList(){
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-    .then((response)=>{
-      return (response).json()
-    }).then(result => {
-      SetPokemon(result.results)
+  // Je recupere la liste des 151 premiers pokemon
+  function pokemonList() {
+    // Je requete l'api avec les 151 premiers
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((response) => {
+        return response.json();
+      })
+      .then((allPokemon) => {
+        // Je boucle sur la liste des pokemons
+        allPokemon.results.map((pokemon) => {
+          return fetchPokemonComplet(pokemon);
+        });
+      });
+  }
+
+  // Je recupere la totalité des infos des pokemons
+  function fetchPokemonComplet(pokemon){
+    const fullPokemon = {};
+    // Je stock l'url du pokemon
+    const url = pokemon.url;
+    // Je stock le nom du pokemon
+    const name = pokemon.name;
+    // Je requete l'url de chez pokemon pour avoir les infos de chaque pokemon
+    fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      
+      // Je recupere la photo de chaque pokemon
+      fullPokemon.picture = data.sprites.front_default;
+      // Je recupere le type de chaque pokemon
+      fullPokemon.type = data.types[0].type.name;
+      // console.log(fullPokemon)
+
+      // Je recupere les noms francais des pokemons
+      fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}`)
+      .then(response => response.json())
+      .then((frenchName) => {
+        // console.log(frenchName.names[4])
+        fullPokemon.name = frenchName.names[4].name
+        // Je pousse dans le state les infos du pokemon actuel en preservant les infos des pokemons precedent
+        SetAllPokemon(allPokemon => [...allPokemon, fullPokemon])
+      })
     })
   }
 
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 
 import './navbar.scss'
 
 const Navbar = () => {
 
+  const [active, setActive] = useState(false)
+
   return (
     <div className="navbar">
       <h1 className='pokedex'>
         <Link to='/'>Pokedex</Link>
       </h1>
+      <form className={active ? 'search-poke-active' : 'search-poke'}>
+        <label
+          htmlFor='search'
+        >
+          Tu cherche un pokemon ?
+        </label>
+        <input
+          type='text'
+          id='search'
+          onInput={!setActive}
+        ></input>
+        <button type='submit' >Rechercher</button>
+      </form>
     </div>
   )
 }
